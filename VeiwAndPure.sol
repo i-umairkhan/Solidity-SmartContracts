3b45216ca60a1035e596b21473e553d598d95e18
Umair Khan <81556052+umair2607@users.noreply.github.com>
2022-02-25 23:55:14 +0500

    Update and rename VeiwAndPure.sol to keywords.sol

--- VeiwAndPure.sol
+++
@@ -1,4 +0,0 @@
-// 1- View keyword is used with a function when it returns a not . It can not modify a value .
-// 2- Pure keyword is used with a function when it returns only a computation value.
-// 3- If none of above keyword is used with a function then values can be modified inside a function.
-
---
+++ keywords.sol
@@ -0,0 +1,10 @@
+FUNCTION MODIFIERS .
+1- (View) keyword is used with a function when it returns a not . It can not modify a value .
+2- (Pure) keyword is used with a function when it returns only a computation value.
+3- If none of above keyword is used with a function then values can be modified inside a function.
+
+FUNCTION VISIBILTY (also apply to varriables visisbilty) .
+1- (Private) can be called only from inside in which defined, by default every function is private.
+2- (Internel)  can be called only from inside and contracts derived from it.
+3- (Externel) can be from outside only.
+4- (Public) can be called internely or externely.
