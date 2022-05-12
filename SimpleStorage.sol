c3d893b00fba7721a19b00dbac07d3d5e649159b
Umair Khan <81556052+umair2607@users.noreply.github.com>
2022-05-13 01:35:53 +0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
 contract SimpleStorage{
@@ -11,5 +12,8 @@ contract SimpleStorage{
     function addPeople(uint256 _num,string memory _name) public{
         allPeoplels.push(People(_num,_name));
         myPeopels[_name] = _num;
+    }
+    function getPeople(uint256 _index)public view returns  (string memory){
+        return allPeoplels[_index].name;
     }
 }   
